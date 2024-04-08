2e7bd3a5ab8bbdbc63c956bf55f0e92ccffd9eae
Pedro Luz <pedroluz895@gmail.com>
2024-04-08 11:22:35 -0300

    Correcoes

--- funcoes.py
+++ funcoes.py
@@ -15,26 +15,39 @@ def search(file, path):
 
 
 def aquecer(df, df_leads, cont_num, chrome_path, brave_path, edge_path, opera_path, WIDTH, HEIGHT ):
-    navegadores = [chrome_path, brave_path, edge_path, opera_path]
-    tp_envio = random.randint(1, 10)  # 30seg a 3min
-    choice = random.randint(0, 3)
-    choice_is_equal = 0
+    nav = [chrome_path, brave_path, edge_path, opera_path]
+    nn = 0
+    navegadores = []
+    tp_envio = random.randint(10, 15)
+
     print(f"Tempo para envio de mensagem: {tp_envio} Segundos.")
-    time.sleep(tp_envio)
-    if choice_is_equal == choice:
-        if choice <= 1:
-            choice = choice + 1
-        if choice > 1:
-            choice = choice - 1
+    # time.sleep(tp_envio)
+    for n in range(len(nav)):
+        print(nav[n])
+        if nav[n] != '':
+            navegadores.append(nav[n])
+            nn += 1
+    max_nav = len(navegadores) - 1
+    choice = random.randint(0, max_nav)
+    choice_is_equal = 0
+    if max_nav > 0:
+        if choice_is_equal == choice:
+            if choice <= 1:
+                choice = choice + 1
+            if choice > 1:
+                choice = choice - 1
+
 
     for c in range(cont_num):
-        webbrowser.get(navegadores[choice]).open("https://web.whatsapp.com/send?phone=" + df_leads[c])
+        ms = df_leads[c]
+        webbrowser.get(navegadores[choice]).open("https://web.whatsapp.com/send?phone=" + ms)
         print("ENVIANDO MENSAGEM...")
-        time.sleep(10)
+        time.sleep(5 + tp_envio)
         pg.click(WIDTH / 2, HEIGHT / 2)
         pg.typewrite(df['msg'].sample(ignore_index=True)[0])
+        time.sleep(5)
         pg.press('enter')
-        time.sleep(4)
+        time.sleep(2)
         pg.hotkey('ctrl', 'w')
         choice_is_equal = choice
 
